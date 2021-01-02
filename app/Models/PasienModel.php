393c57483bec4312ca6b2ad87b2c11d174444f1c
Henrico Leodra <leodra28@gmail.com>
2021-01-02 20:29:11 +0700

    [FIX] Kota Kecamatan

--- app/Models/PasienModel.php
+++ app/Models/PasienModel.php
@@ -8,5 +8,5 @@ class PasienModel extends Model
 {
   protected $table = 'mtbs_pasien';
   protected $primaryKey = 'idPasien';
-  protected $allowedFields = ['namaIbu', 'namaAnak', 'tanggalLahir', 'provinsi', 'alamat', 'rt', 'rw', 'kelurahanDesa', 'kotaKecamatan'];
+  protected $allowedFields = ['namaIbu', 'namaAnak', 'tanggalLahir', 'provinsi', 'kota', 'alamat', 'rt', 'rw', 'kelurahanDesa', 'kecamatan'];
 }
